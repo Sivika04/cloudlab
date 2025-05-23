704b9f82b0b7f5ac51f4cb8f7304ae8337b4a9cf
Sivika04 <153796550+Sivika04@users.noreply.github.com>
2025-05-23 15:15:50 +0530

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,7 +1,7 @@
 from collections import Counter
 def count_elements(data):
     return Counter(data)
-//if __name__ == "__main__":
+
    
     my_string = "cloudlab"
     string_counts = count_elements(my_string)
