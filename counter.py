2d86100c5df0bccad74933a4c1f86c8c4603ab25
Sivika04 <153796550+Sivika04@users.noreply.github.com>
2025-05-23 15:15:17 +0530

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,8 +1,8 @@
 from collections import Counter
 def count_elements(data):
     return Counter(data)
-if __name__ == "__main__":
+//if __name__ == "__main__":
    
-    my_string = "abracadabra"
+    my_string = "cloudlab"
     string_counts = count_elements(my_string)
     print("String character counts:", string_counts)
